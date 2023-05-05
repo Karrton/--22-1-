0e54804c6e66b70b3fd7f0575a1fe1571f9551c3
Merzlyakov Vladimir RIS-22-1b <115933088+Karrton@users.noreply.github.com>
2023-05-05 23:53:16 +0500

    Add files via upload

---
+++ Lab-6 (Class)/Лабораторная работа №6 (Классы)/Лабораторная работа №6 (Классы)/Лабораторная работа №6 (Классы).cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+#include "List.h"
+
+int main() {
+    List<int> lst;
+    lst.push_back(1);
+    lst.push_back(2);
+    lst.push_back(4);
+    std::cout << "Size of range - " << lst.size() << std::endl;
+    std::cout << "List[0] * List[1] = " << lst[0] * lst[1] << std::endl;
+    List<int>::Iterator a = lst.begin();
+    std::cout << "a+3 = " << *(a + 3) << std::endl;
+    int i = 0;
+    for (List<int>::Iterator it = lst.begin(); it != lst.end(); ++it) 
+        std::cout << "List[" << i++ << "] = " << *it << std::endl;
+    return 0;
+}
---
+++ Lab-7 (Class)/Лабораторная работа №7 (Классы)/Лабораторная работа №6 (Классы)/Лабораторная работа №6 (Классы).cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+#include "List.h"
+#include "Pair.h"
+
+int main() {
+    List<pair> lst;
+    pair d(3, 2.34);
+    pair s(4, 3.34);
+    pair m(1, 6.34);
+    lst.push_back(d);
+    lst.push_back(s);
+    lst.push_back(m);
+    std::cout << "Size of range - " << lst.size() << std::endl;
+    std::cout << "List[0] * List[1] = " << lst[0] * lst[1] << std::endl;
+    List<pair>::Iterator a = lst.begin();
+    std::cout << "a+3 = " << *(a + 3) << std::endl;
+    int i = 0;
+    for (List<pair>::Iterator it = lst.begin(); it != lst.end(); ++it)
+        std::cout << "List[" << i++ << "] = " << *it << std::endl;
+    return 0;
+}
